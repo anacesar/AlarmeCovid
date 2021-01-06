01238c2944b322df4177980472b23e92b136be80
Stark <margaridafaria23@gmail.com>
2021-01-06 17:08:31 +0000

    changes in class Client

--- src/Data/Data.java
+++ src/Data/Data.java
@@ -1,8 +1,11 @@
 package Data;
 
+import java.net.Socket;
 import java.util.Map;
 
 public class Data {
     private Map<String,User> users;
     private myMap map;
+
+    private Map<String, Socket> userSocket;
 }
